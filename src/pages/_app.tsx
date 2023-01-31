4c8b536638342af500e52580d6a00a1bfa442572
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-01-31 18:11:15 -0300

    add: header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 import { Roboto } from '@next/font/google'
+import logoImg from '../assets/logo.svg'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -11,6 +13,14 @@ const roboto = Roboto({
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className={`${roboto.variable} font-sans`}>
+      <div className='flex flex-col items-start content-center min-h-full'>
+        <header className='py-8 w-full max-w-[1180px] mx-auto'>
+          <Image
+            src={logoImg}
+            alt=''
+          />
+        </header>
+      </div>
       <Component {...pageProps} />
     </main>
   )

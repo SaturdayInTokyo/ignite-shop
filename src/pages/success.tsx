78be8f20fead5685ce0b1e5f4a88e857947a17b7
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-02-10 16:23:26 -0300

    Fix: success checkout page for single product

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -9,9 +9,9 @@ export default function Success({ customerName, product }) {
   function showSuccessPurchaseItems() {
     if (product.length === 1) {
       return (
-        <div>
-          <div className="flex justify-center w-32 h-32 mb-8 rounded-lg box-border bg-gradient-to-b from-[#1ea483] to-[#7465d4]">
-            <Image src={''} width={120} height={110} alt="" />
+        <div className="flex flex-col items-center">
+          <div className="flex justify-center w-40 h-40 mb-8 rounded-full box-border bg-gradient-to-b from-[#1ea483] to-[#7465d4]">
+            <Image src={product[0].price.product.images[0]} width={150} height={110} alt="" />
           </div>
           <h1 className="text-ignite-2xl font-bold mb-16">Compra efetuada!</h1>
           <p className="text-ignite-xl mb-24 text-center w-[30rem]">Uhuul <strong>{customerName}</strong>, sua <strong>{product.description}</strong> já está a caminho de sua casa.</p>
@@ -23,7 +23,7 @@ export default function Success({ customerName, product }) {
           <div className="flex ml-[3.5rem]">
             {product.map(product => {
               return (
-                <div className="flex justify-center ml-[-3.5rem] shadow-[0_60px_40px_-10px_rgba(0,0,0,0.3)] w-40 h-40 mb-8 box-border rounded-full bg-gradient-to-b from-[#1ea483] to-[#7465d4]">
+                <div key={product.id} className="flex justify-center ml-[-3.5rem] shadow-[0_60px_40px_-10px_rgba(0,0,0,0.3)] w-40 h-40 mb-8 box-border rounded-full bg-gradient-to-b from-[#1ea483] to-[#7465d4]">
                   <Image src={product.price.product.images[0]} width={150} height={110} alt="" />
                 </div>
               )
